ab6a450205c733c1610247e1a676b55a88ae48e1
jayandhanrajasundaram <jayandhanr@operative.com>
2018-12-16 13:37:20 +0530

    Changes for Primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'QuantUI';
+
+  public symbol: string;
+  public startDate: string;
+  public endDate: string;
+  
+  title = 'Algoz India';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule }  from 'primeng/calendar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CalendarModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
